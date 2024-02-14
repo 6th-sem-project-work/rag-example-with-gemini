bbb098130f8226715c7efc7d89a4f791d1586280
thrombe <yash.n.thombare620@gmail.com>
2024-02-14 23:09:08 +0530

    set up a flake for development shell

---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
+      config = {
+        allowUnfree = true;
+        # cudaSupport = true;
+      };
+
+      overlay-unstable = final: prev: {
+        unstable = import inputs.nixpkgs-unstable {
+          inherit system config;
+        };
+      };
+      pkgs = import inputs.nixpkgs {
+        inherit system config;
+        overlays = [
+          overlay-unstable
+        ];
+      };
+
+      fhs = pkgs.buildFHSEnv {
+        name = "fhs-shell";
+        targetPkgs = p: (packages p) ++ [chatmed-serve];
+        runScript = "${pkgs.zsh}/bin/zsh";
+        profile = ''
+          source ./.venv/bin/activate
+          source .env
+        '';
+      };
+      chatmed-serve = pkgs.buildFHSEnv {
+        name = "chatmed-serve";
+        targetPkgs = packages;
+        runScript = ''
+          #!/usr/bin/env bash
+          source ./.venv/bin/activate
+          # pip install -r ./requirements.txt
+          streamlit run ./src/app.py
+        '';
+      };
+
+      packages = pkgs: (with pkgs; [
+        (pkgs.python310.withPackages (ps:
+          with ps; [
+          ]))
+        python311Packages.python-lsp-server
+        python311Packages.ruff-lsp # python linter
+        python311Packages.black # python formatter
+
+        pkgs.python310Packages.pip
+        pkgs.python310Packages.virtualenv
+        zlib # idk why/if this is needed
+
+        redis
+
+        # virtualenv .venv
+        # source ./.venv/bin/activate
+        # pip install ..
+        # python311Packages.venvShellHook # ??
+      ]);
+    in {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = [fhs chatmed-serve] ++ packages pkgs;
+        shellHook = ''
+          source .env
+        '';
+      };
+      # devShells.default = fhs.env;
+    });
+}

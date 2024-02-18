5d6a37e0185d057919d498b8c68adba9ba028277
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2024-02-18 21:23:37 +0530

    created route for response

--- src/api_main.py
+++ src/api_main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI
 import redis
 import json
+from qa import QAResponse
 
 rc = redis.Redis(host='localhost', port=6379, decode_responses=True)
 app = FastAPI()
@@ -15,4 +16,9 @@ def get_message_history(client_id: str):
         history = list(map(json.loads, history))
         return { "history": history }
 
+@app.get("/response")
+def get_user_response(query: str):
+    qaobject = QAResponse()
+    response = qaobject.get_response(query=query)
+    return {"user": query, "response": response}
 
